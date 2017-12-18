961f6a9d9fba049300b11d45501bf8372ac54f26
apazacoder(desktop) <apazaalcides@gmail.com>
2017-12-18 16:16:15 -0400

    Inicial

---
+++ patrocinio.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title></title>
+	<meta name="description" content="">
+	<link rel="apple-touch-icon" href="apple-touch-icon.png">
+	<link rel="shortcut icon" href="favicon.png" type="image/png">
+
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<script src="js/vendor/modernizr-2.8.3.min.js"></script>
+	<!--[if lt IE 9]>
+	<script src="js/html5shiv_min.js"></script>
+	<script src="js/respond_min.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+	<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+					aria-expanded="false" aria-controls="navbar">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#">UN MILLÓN DE LÍNEAS</a>
+		</div>
+		<div id="navbar" class="navbar-collapse collapse">
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#">¿QUIENES SOMOS?<span class="sr-only">(current)</span></a></li>
+				<li><a href="#">INVOLÚCRATE</a></li>
+			</ul>
+		</div><!--/.navbar-collapse -->
+	</div>
+</nav>
+
+<div class="container text-center">
+	<iframe src="https://docs.google.com/forms/d/e/1FAIpQLScdhEc3uVraVkmyrg7WQBqwEk2WZwby3_dKLNTC1dENwmfVWw/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Cargando...</iframe>
+	<hr>
+
+	<footer>
+		<p>&copy; 2016 Company, Inc.</p>
+	</footer>
+</div> <!-- /container -->
+
+
+<script src="js/vendor/jquery-1.12.0.min.js"></script>
+<script src="js/plugins.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
+
+
